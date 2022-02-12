bbf26aee186b4b187910402f73f8c472664e0cad
tan272 <54320718+tan272@users.noreply.github.com>
2022-02-12 16:32:11 -0500

    Trying to add unit/integration/service test to repos

---
+++ tests/test_flask.py
@@ -0,0 +1,26 @@
+import unittest
+from app import app
+import requests
+import json
+import sys
+
+class TestFlaskApiUsingRequests(unittest.TestCase):
+    def test_hello_world(self):
+        response = requests.get('http://localhost:5000')
+        self.assertEqual(response.json(), {'hello': 'world'})
+
+
+class TestFlaskApi(unittest.TestCase):
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_hello_world(self):
+        response = self.app.get('/')
+        self.assertEqual(
+            json.loads(response.get_data().decode(sys.getdefaultencoding())), 
+            {'hello': 'world'}
+        )
+
+
+if __name__ == "__main__":
+    unittest.main()
