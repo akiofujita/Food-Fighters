07cebad94579bdc758f978bd7f132f0882a7c914
tan272 <54320718+tan272@users.noreply.github.com>
2022-02-12 16:58:53 -0500

    Trying unittest

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -7,23 +7,10 @@ sys.path.append('.')
 
 import backend.app as app
 
-class TestFlaskApiUsingRequests(unittest.TestCase):
+class TestHelloWorld(unittest.TestCase):
     def test_hello_world(self):
-        response = requests.get('http://localhost:5000')
-        self.assertEqual(response.json(), {'hello': 'world'})
-
-
-class TestFlaskApi(unittest.TestCase):
-    def setUp(self):
-        self.app = app.test_client()
-
-    def test_hello_world(self):
-        response = self.app.get('/')
-        self.assertEqual(
-            json.loads(response.get_data().decode(sys.getdefaultencoding())), 
-            {'hello': 'world'}
-        )
-
+        response = self.get('/')
+        self.assertEqual(response, "<h1>Homepage</h1>")
 
 if __name__ == "__main__":
     unittest.main()
