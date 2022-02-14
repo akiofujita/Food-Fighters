e748e1a3198950319b01601d31e2e286fef6df5d
Rachael Tan <tan272@purdue.edu>
2022-02-14 00:45:44 -0500

    I Just Want A Connection

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,24 +6,11 @@ sys.path.append('.')
 import backend.project as project
 
 @pytest.fixture()
-def app():
-    app = project.create_app()
-    app.config.update({
-        "TESTING": True,
-    })
+def test_client():
+    flask_app = project.create_app('flask_test.cfg')
 
-    # other setup can go here
-
-    yield app
-
-    # clean up / reset resources here
-
-
-@pytest.fixture()
-def client(app):
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    return app.test_cli_runner()
+    # Create a test client using the Flask application configured for testing
+    with flask_app.test_client() as testing_client:
+        # Establish an application context
+        with flask_app.app_context():
+            yield testing_client  # this is where the testing happens!
