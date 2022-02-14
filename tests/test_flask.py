b2f9a8dd335b84895e41d91c6c535357d2c98b50
Rachael Tan <tan272@purdue.edu>
2022-02-13 23:13:58 -0500

    Pain

--- tests/test_flask.py
+++ tests/test_flask.py
@@ -6,7 +6,6 @@ sys.path.append('.')
 import backend.app as app
 
 def test_homepage():
-    with app.test_client() as test_client:
-        response = test_client.get('/')
-        assert response.status_code == 200
-        assert "<h1>Homepage</h1>" in response.data
+    response = app.get('/')
+    assert response.status_code == 200
+    assert "<h1>Homepage</h1>" in response.data
