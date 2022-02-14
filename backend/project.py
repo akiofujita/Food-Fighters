29303173ba65d301e1c97e5a6d6d02d839bbcb79
Rachael Tan <tan272@purdue.edu>
2022-02-14 00:13:37 -0500

    Trying no cfg

--- backend/project.py
+++ backend/project.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 
 #######################
 #### Configuration ####
@@ -8,7 +8,7 @@ from flask import Flask
 # Create the instances of the Flask extensions (flask-sqlalchemy, flask-login, etc.) in
 # the global scope, but without any arguments passed in.  These instances are not attached
 # to the application at this point.
-# db = SQLAlchemy()
+db = SQLAlchemy()
 # login = LoginManager()
 # login.login_view = "users.login"
 
@@ -20,4 +20,5 @@ from flask import Flask
 def create_app(config_filename=None):
     app = Flask(__name__, instance_relative_config=True)
     #app.config.from_pyfile(config_filename)
+    db.init_app(app)
     return app
