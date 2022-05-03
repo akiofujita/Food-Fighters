06ba4961a88a86187f2ebb001794bde60d080853
tan272 <54320718+tan272@users.noreply.github.com>
2022-05-03 14:48:21 -0400

    Issue with google cloud and whitelisting

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,11 +1,9 @@
-""" Checks if CI can connect and properly query from database """
+""" Checks if CI can connect and properly query from database 
 import mysql.connector
 def test_db():
-  """
   Using cursor, connect to database service and query a count of recipes.
   If counts are > 0, then db is populated and should query properly.
-  Only recipes are taken into account becase if recipes don't exist it cascades
-  """
+  Only recipes are taken into account becase if recipes don't exist it cascade
   cnx = mysql.connector.connect(user='root', password='ffDB2022!', host = '34.72.233.63', database='FoodFighters')
 
   cursor = cnx.cursor(buffered=True)
@@ -15,3 +13,4 @@ def test_db():
   cnx.commit()
   cursor.close()
   assert (count > 0)
+"""
